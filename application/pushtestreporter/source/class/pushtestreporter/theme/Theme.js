289c528fc1886f7c9865cc5f7e641dc1dc95cfe8
Andreas Parusel <andreas.parusel@gmail.com>
2012-09-15 18:52:23 +0200

    Added skeleton for Reporter-app

---
+++ application/pushtestreporter/source/class/pushtestreporter/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("pushtestreporter.theme.Theme",
+{
+  meta :
+  {
+    color : pushtestreporter.theme.Color,
+    decoration : pushtestreporter.theme.Decoration,
+    font : pushtestreporter.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : pushtestreporter.theme.Appearance
+  }
+});
